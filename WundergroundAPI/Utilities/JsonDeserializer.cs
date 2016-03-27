73ca4192389a4cf5e65a4a9516297e3f0b2f47a3
Martin Kunc <martin.kunc@hotmail.com>
2016-03-27 13:41:25 +0200

    Add Configurable Json.Net deserializer

---
+++ WundergroundAPI/Utilities/JsonDeserializer.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Deserializers;
+using System.Globalization;
+
+namespace CGurus.Weather.WundergroundAPI.Utilities
+{
+
+    public class JsonDeserializer : IDeserializer
+    {
+        private readonly Newtonsoft.Json.JsonSerializer _serializer;
+        public JsonConverter[] JsonConverters;
+
+        public string RootElement { get; set; }
+        public string Namespace { get; set; }
+        public string DateFormat { get; set; }
+
+        public CultureInfo Culture { get; set; }
+
+        public JsonDeserializer()
+        {
+            this.Culture = CultureInfo.InvariantCulture;
+        }
+
+        public T Deserialize<T>(IRestResponse response)
+        {
+            return JsonConvert.DeserializeObject<T>(response.Content, JsonConverters);
+        }
+
+    }
+}
--- WundergroundAPI/Utilities/RestRequest.cs
+++ WundergroundAPI/Utilities/RestRequest.cs
@@ -15,17 +15,16 @@ namespace CGurus.Weather.WundergroundAPI.Utilities
             RestSharp.IRestRequest request = new RestSharp.RestRequest();
 
             var client = new RestClient();
+            var jsonDeserializer = new JsonDeserializer();
+            jsonDeserializer.JsonConverters = new JsonConverter[] { new Utilities.BoolConverter(), new Utilities.DoubleConverter() };
+            client.AddHandler("application/json", jsonDeserializer);
+
             client.BaseUrl = Uri;
             var response = client.Execute<T>(request);
 
             if (response.ErrorException != null)
             {
                 throw response.ErrorException;
-            }
-
-            if (response.Content.Length > 0)
-            {
-                return JsonConvert.DeserializeObject<T>(response.Content, new Utilities.BoolConverter(), new Utilities.DoubleConverter());
             }
 
             return response.Data;
