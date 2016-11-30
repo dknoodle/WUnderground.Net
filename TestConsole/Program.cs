b233c60d38a73e5e9ce8ae3750a580ec2a078b42
Jesse Barocio <jesse@barocio.net>
2016-11-29 20:25:41 -0700

    Created methods that accept lat/long coordinates.

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -15,13 +15,17 @@ namespace TestConsole
 
             var alerts = wApi.GetAlertsUS("CA", "San Francisco");
             var alertsPws = wApi.GetAlertsUS("KUTCLEAR3");
+            var alertsLatLong = wApi.GetAlertsUS(40.777222, -111.888056);
             var forecast = wApi.GetForecastUS("CA", "San Francisco");
-            var forecastPws = wApi.GetAlertsUS("KUTCLEAR3");
+            var forecastPws = wApi.GetForecastUS("KUTCLEAR3");
+            var forecastLatLong = wApi.GetForecastUS(40.777222, -111.888056);
             var forecast10Day = wApi.GetForecast10DayUS("CA", "San Francisco");
             var forecast10DayPws = wApi.GetForecast10DayUS("KUTCLEAR3");
+            var forecast10DayLatLong = wApi.GetForecast10DayUS(40.777222, -111.888056);
             var forecastHourlyUS = wApi.GetForecastHourlyUS("CA", "San Francisco");
             var forecastHourlyPws = wApi.GetForecastHourlyUS("KUTCLEAR3");
-            
+            var forecastHourlyLatLong = wApi.GetForecastHourlyUS(40.777222, -111.888056);
+
             Console.ReadLine();
 
         }
