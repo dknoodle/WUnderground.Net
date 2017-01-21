9ca5379fb2527c5ad002662c727a33913a72ee27
Daniel Knoodle <dknoodle@gmail.com>
2017-01-21 16:20:37 -0600

    Added Async methods for GeoLocation.

--- CreativeGurus.Weather.Wunderground/Services/GeoLookup.cs
+++ CreativeGurus.Weather.Wunderground/Services/GeoLookup.cs
@@ -10,13 +10,82 @@ namespace CreativeGurus.Weather.Wunderground.Services
 {
     public class GeoLookup
     {
-        private string _baseUrl;
         private string _apiKey;
-
+        private string _baseUrl;
         internal GeoLookup(string apiKey, string baseUrl)
         {
             this._apiKey = apiKey;
             this._baseUrl = baseUrl;
+        }
+
+        public GeoLookupData GeoLookupAirportCode(string airportCode)
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/{2}.json", _baseUrl, _apiKey, airportCode);
+
+            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
+        }
+
+        public async Task<GeoLookupData> GeoLookupAirportCodeAsync(string airportCode)
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/{2}.json", _baseUrl, _apiKey, airportCode);
+
+            return await RestRequest.ExecuteAsync<GeoLookupData>(new Uri(uri)).ConfigureAwait(false);
+        }
+
+        public GeoLookupData GeoLookupAutoIPAddress()
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/autoip.json", _baseUrl, _apiKey);
+
+            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
+        }
+
+        public async Task<GeoLookupData> GeoLookupAutoIPAddressAsync()
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/autoip.json", _baseUrl, _apiKey);
+
+            return await RestRequest.ExecuteAsync<GeoLookupData>(new Uri(uri)).ConfigureAwait(false);
+        }
+
+        public GeoLookupData GeoLookupGlobal(string country, string city)
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/{2}/{3}.json", _baseUrl, _apiKey, country, city.Replace(" ", "_"));
+
+            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
+        }
+
+        public async Task<GeoLookupData> GeoLookupGlobalAsync(string country, string city)
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/{2}/{3}.json", _baseUrl, _apiKey, country, city.Replace(" ", "_"));
+
+            return await RestRequest.ExecuteAsync<GeoLookupData>(new Uri(uri)).ConfigureAwait(false);
+        }
+
+        public GeoLookupData GeoLookupLatLon(string latitude, string longitude)
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/{2},{3}.json", _baseUrl, _apiKey, latitude, longitude);
+
+            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
+        }
+
+        public async Task<GeoLookupData> GeoLookupLatLonAsync(string latitude, string longitude)
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/{2},{3}.json", _baseUrl, _apiKey, latitude, longitude);
+
+            return await RestRequest.ExecuteAsync<GeoLookupData>(new Uri(uri)).ConfigureAwait(false);
+        }
+
+        public GeoLookupData GeoLookupPersonalWeatherStation(string weatherStationId)
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/pws:{2}.json", _baseUrl, _apiKey, weatherStationId);
+
+            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
+        }
+
+        public async Task<GeoLookupData> GeoLookupPersonalWeatherStationAsync(string weatherStationId)
+        {
+            string uri = string.Format("{0}/{1}/geolookup/q/pws:{2}.json", _baseUrl, _apiKey, weatherStationId);
+
+            return await RestRequest.ExecuteAsync<GeoLookupData>(new Uri(uri)).ConfigureAwait(false);
         }
 
         public GeoLookupData GeoLookupUS(string city, string state)
@@ -28,20 +97,14 @@ namespace CreativeGurus.Weather.Wunderground.Services
             return RestRequest.Execute<GeoLookupData>(new Uri(uri));
         }
 
-        public GeoLookupData GeoLookupGlobal(string country, string city)
+        public async Task<GeoLookupData> GeoLookupUSAsync(string city, string state)
         {
-            string uri = string.Format("{0}/{1}/geolookup/q/{2}/{3}.json", _baseUrl, _apiKey, country, city.Replace(" ", "_"));
+            Validation.ValidateState(state);
 
-            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
+            string uri = string.Format("{0}/{1}/geolookup/q/{2}/{3}.json", _baseUrl, _apiKey, state, city.Replace(" ", "_"));
+
+            return await RestRequest.ExecuteAsync<GeoLookupData>(new Uri(uri)).ConfigureAwait(false);
         }
-
-        public GeoLookupData GeoLookupAutoIPAddress()
-        {
-            string uri = string.Format("{0}/{1}/geolookup/q/autoip.json", _baseUrl, _apiKey);
-
-            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
-        }
-
         public GeoLookupData GeoLookupUSZipCode(string zipCode)
         {
             string uri = string.Format("{0}/{1}/geolookup/q/{2}.json", _baseUrl, _apiKey, zipCode);
@@ -49,25 +112,11 @@ namespace CreativeGurus.Weather.Wunderground.Services
             return RestRequest.Execute<GeoLookupData>(new Uri(uri));
         }
 
-        public GeoLookupData GeoLookupAirportCode(string airportCode)
+        public async Task<GeoLookupData> GeoLookupUSZipCodeAsync(string zipCode)
         {
-            string uri = string.Format("{0}/{1}/geolookup/q/{2}.json", _baseUrl, _apiKey, airportCode);
+            string uri = string.Format("{0}/{1}/geolookup/q/{2}.json", _baseUrl, _apiKey, zipCode);
 
-            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
-        }
-
-        public GeoLookupData GeoLookupLatLon(string latitude, string longitude)
-        {
-            string uri = string.Format("{0}/{1}/geolookup/q/{2},{3}.json", _baseUrl, _apiKey, latitude, longitude);
-
-            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
-        }
-
-        public GeoLookupData GeoLookupPersonalWeatherStation(string weatherStationId)
-        {
-            string uri = string.Format("{0}/{1}/geolookup/q/pws:{2}.json", _baseUrl, _apiKey, weatherStationId);
-
-            return RestRequest.Execute<GeoLookupData>(new Uri(uri));
+            return await RestRequest.ExecuteAsync<GeoLookupData>(new Uri(uri)).ConfigureAwait(false);
         }
     }
 }
