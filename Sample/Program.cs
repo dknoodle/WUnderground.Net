dab0da4b9dd18467c906beb9af0d5df9b9893bbd
Daniel Knoodle <dknoodle@gmail.com>
2017-01-27 10:25:20 -0600

    Minor formatting and example fixes

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -14,31 +14,30 @@ namespace Sample
 
         private static async Task<bool> Examples()
         {
-			try
-			{
+            try
+            {
                 //List of supported languages
                 //https://www.wunderground.com/weather/api/d/docs?d=language-support&MR=1
 
-                string key = "611649331a9c9e34";
-                //string key = "{Your API Key Here}";
+                string key = "{Your API Key Here}";
 
-				WeatherClient client = new WeatherClient(key);
+                WeatherClient client = new WeatherClient(key);
 
-				//Sample Alerts
-				//var a1 = client.GetAlerts(QueryType.AirportCode, new QueryOptions() { AirportCode = "DFW" });  //Gets data by Airport Code
-				//var a2 = client.GetAlerts(QueryType.AutoIp);  //Gets data by the detected IP address of the calling client
-				//var a3 = client.GetAlerts(QueryType.GlobalCity, new QueryOptions() { Country = "France", City = "Paris"}); //Gets data by country and city
-				//var a4 = client.GetAlerts(QueryType.GPS, new QueryOptions() { Latitude = "37.776289", Longitude = "-122.395234" }); //Gets data by GPS coordinates
-				//var a5 = client.GetAlerts(QueryType.LinkId, new QueryOptions() { LinkId = "00000.1.16172" }); //Gets data by Weather Underground Link ID
-				//var a6 = client.GetAlerts(QueryType.PWSId, new QueryOptions() { PWSId = "KMNCHASK10" }); //Gets data from a personal weather station by ID
-				//var a7 = client.GetAlerts(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" }); //Gets data for a US City
-				//var a8 = client.GetAlerts(QueryType.ZipCode, new QueryOptions() { ZipCode = "94107" }); //Gets data by US Zip Code
+                //Sample Alerts
+                //var a1 = client.GetAlerts(QueryType.AirportCode, new QueryOptions() { AirportCode = "DFW" });  //Gets data by Airport Code
+                //var a2 = client.GetAlerts(QueryType.AutoIp);  //Gets data by the detected IP address of the calling client
+                //var a3 = client.GetAlerts(QueryType.GlobalCity, new QueryOptions() { Country = "France", City = "Paris"}); //Gets data by country and city
+                //var a4 = client.GetAlerts(QueryType.GPS, new QueryOptions() { Latitude = "37.776289", Longitude = "-122.395234" }); //Gets data by GPS coordinates
+                //var a5 = client.GetAlerts(QueryType.LinkId, new QueryOptions() { LinkId = "00000.1.16172" }); //Gets data by Weather Underground Link ID
+                //var a6 = client.GetAlerts(QueryType.PWSId, new QueryOptions() { PWSId = "KMNCHASK10" }); //Gets data from a personal weather station by ID
+                //var a7 = client.GetAlerts(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" }); //Gets data for a US City
+                //var a8 = client.GetAlerts(QueryType.ZipCode, new QueryOptions() { ZipCode = "94107" }); //Gets data by US Zip Code
 
-				//Sample Alminac
-				//var al1 = client.GetAlmanac(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" });
+                //Sample Alminac
+                //var al1 = client.GetAlmanac(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" });
 
-				//Sample Astronomy
-				//var as1 = client.GetAstronomy(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" });
+                //Sample Astronomy
+                //var as1 = client.GetAstronomy(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" });
 
                 //Sample Forecasts
                 //var f1 = client.GetForecast(QueryType.AirportCode, new QueryOptions() { AirportCode = "DFW" });  //Gets data by Airport Code
@@ -60,11 +59,18 @@ namespace Sample
                 //var g7 = client.GetGeoLookup(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" }); //Gets data for a US City
                 //var g8 = client.GetGeoLookup(QueryType.ZipCode, new QueryOptions() { ZipCode = "94107" }); //Gets data by US Zip Code
 
-				//Sample Hourly
-				//var h1 = client.GetHourly(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" });
+                //Sample Hourly
+                //var h1 = client.GetHourly(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" });
 
-				//Sample Conditions
-				//var c1 = await client.GetConditionsAsync(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" });
+                //Sample Conditions
+                //var c1 = await client.GetConditionsAsync(QueryType.USCity, new QueryOptions() { City = "Dallas", State = "TX" });
+
+                await client.GetAlertsAsync(QueryType.USCity, new QueryOptions() { City = "Las Angeles", State = "CA" });
+
+
+                client.GetForecast(QueryType.AutoIp, new QueryOptions() { Language = "FR" };
+                client.GetForecast(QueryType.AutoIp, new QueryOptions() { UsePWS = false };
+                client.GetForecast(QueryType.AutoIp, new QueryOptions() { UseBestFct = false };
 
                 return true;
             }
