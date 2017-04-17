90d753d52cf0171b89ed9cbbbc3e4777878af7c7
Daniel Knoodle <dknoodle@gmail.com>
2017-04-17 10:49:57 -0500

    Added additional validation to UrlBuilder and removed unused Try/Catch wrapper.

--- CreativeGurus.Weather.Wunderground/Utilities/UrlBuilder.cs
+++ CreativeGurus.Weather.Wunderground/Utilities/UrlBuilder.cs
@@ -5,68 +5,71 @@ using System.Text;
 
 namespace CreativeGurus.Weather.Wunderground
 {
-	internal static class UrlBuilder
-	{
-		internal static string BuildUrl(string baseUrl, string apiKey, Feature feature, QueryType queryType, QueryOptions options)
-		{
-            try
+    internal static class UrlBuilder
+    {
+        internal static string BuildUrl(string baseUrl, string apiKey, Feature feature, QueryType queryType, QueryOptions options)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("{0}/{1}/{2}", baseUrl, apiKey, feature.ToString().ToLower());
+
+            if (!string.IsNullOrWhiteSpace(options?.Language)) { sb.AppendFormat("/lang:{0}", options?.Language); }  //Language
+            if (options?.UsePWS == true) { sb.AppendFormat("/pws:{0}", options?.UsePWS); } // Use Personal weather station
+            if (options?.UseBestFct == true) { sb.AppendFormat("/bestfct:{0}", options?.UseBestFct); } // Use weather underground best forecast for forecast
+
+            sb.Append("/q/");
+
+            switch (queryType)
             {
-                StringBuilder sb = new StringBuilder();
+                case QueryType.AirportCode:
+                    if (string.IsNullOrWhiteSpace(options?.AirportCode)) { throw new ArgumentException("AirportCode must be supplied when using a QueryType of AirportCode"); }
+                    sb.Append(options?.AirportCode);
+                    break;
 
-                sb.AppendFormat("{0}/{1}/{2}", baseUrl, apiKey, feature.ToString().ToLower());
+                case QueryType.AutoIp:
+                    sb.Append("autoip");
+                    break;
 
-                if (!string.IsNullOrWhiteSpace(options?.Language)) { sb.AppendFormat("/lang:{0}", options?.Language); }  //Language
-                if (options?.UsePWS == true) { sb.AppendFormat("/pws:{0}", options?.UsePWS); } // Use Personal weather station
-                if (options?.UseBestFct == true) { sb.AppendFormat("/bestfct:{0}", options?.UseBestFct); } // Use weather underground best forecast for forecast
+                case QueryType.LinkId:
+                    if (string.IsNullOrWhiteSpace(options?.LinkId)) { throw new ArgumentException("LinkId must be supplied when using a QueryType of LinkId"); }
+                    sb.AppendFormat("zmw:{0}", options?.LinkId);
+                    break;
 
-                sb.Append("/q/");
+                case QueryType.GlobalCity:
+                    if (string.IsNullOrWhiteSpace(options?.Country)) { throw new ArgumentException("Country must be supplied when using a QueryType of GlobalCity"); }
+                    if (string.IsNullOrWhiteSpace(options?.City)) { throw new ArgumentException("City must be supplied when using a QueryType of GlobalCity"); }
+                    sb.AppendFormat("{0}/{1}", options?.Country, options?.City.Replace(" ", "_"));
+                    break;
 
-                switch (queryType)
-                {
-                    case QueryType.AirportCode:
-                        sb.Append(options?.AirportCode);
-                        break;
+                case QueryType.GPS:
+                    if (string.IsNullOrWhiteSpace(options?.Latitude)) { throw new ArgumentException("Latitude must be supplied when using a QueryType of GPS"); }
+                    if (string.IsNullOrWhiteSpace(options?.Longitude)) { throw new ArgumentException("Longitude must be supplied when using a QueryType of GPS"); }
+                    sb.AppendFormat("{0},{1}", options?.Latitude, options?.Longitude);
+                    break;
 
-                    case QueryType.AutoIp:
-                        sb.Append("autoip");
-                        break;
+                case QueryType.USCity:
+                    if (string.IsNullOrWhiteSpace(options?.City)) { throw new ArgumentException("City must be supplied when using a QueryType of USCity"); }
+                    if (string.IsNullOrWhiteSpace(options?.State)) { throw new ArgumentException("State must be supplied when using a QueryType of USCity"); }
+                    Validation.ValidateState(options?.State);
+                    sb.AppendFormat("{0}/{1}", options?.State, options?.City.Replace(" ", "_"));
+                    break;
 
-                    case QueryType.LinkId:
-                        sb.AppendFormat("zmw:{0}", options?.LinkId);
-                        break;
+                case QueryType.PWSId:
+                    if (string.IsNullOrWhiteSpace(options?.PWSId)) { throw new ArgumentException("PWSId must be supplied when using a QueryType of PWSId"); }
+                    sb.AppendFormat("pws:{0}", options?.PWSId);
+                    break;
 
-                    case QueryType.GlobalCity:
-                        sb.AppendFormat("{0}/{1}", options?.Country, options?.City.Replace(" ", "_"));
-                        break;
-
-                    case QueryType.GPS:
-                        sb.AppendFormat("{0},{1}", options?.Latitude, options?.Longitude);
-                        break;
-
-                    case QueryType.USCity:
-                        Validation.ValidateState(options?.State);
-                        sb.AppendFormat("{0}/{1}", options?.State, options?.City.Replace(" ", "_"));
-                        break;
-
-                    case QueryType.PWSId:
-                        sb.AppendFormat("pws:{0}", options?.PWSId);
-                        break;
-
-                    case QueryType.ZipCode:
-                        sb.Append(options?.ZipCode);
-                        break;
-                }
-
-                sb.Append(".json");
-
-                if (!string.IsNullOrWhiteSpace(options?.IpAddress)) { sb.AppendFormat("?geo_ip={0}", options?.IpAddress); }
-
-                return sb.ToString();
+                case QueryType.ZipCode:
+                    if (string.IsNullOrWhiteSpace(options?.ZipCode)) { throw new ArgumentException("ZipCode must be supplied when using a QueryType of ZipCode"); }
+                    sb.Append(options?.ZipCode);
+                    break;
             }
-            catch (Exception ex)
-            {
-                throw;
-            }
-		}
-	}
+
+            sb.Append(".json");
+
+            if (!string.IsNullOrWhiteSpace(options?.IpAddress)) { sb.AppendFormat("?geo_ip={0}", options?.IpAddress); }
+
+            return sb.ToString();
+        }
+    }
 }
