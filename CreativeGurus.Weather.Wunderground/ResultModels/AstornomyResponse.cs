2f859fb69731ef4d8031c7879a29a14ca1fa5ea9
Daniel Knoodle <dknoodle@gmail.com>
2017-05-23 19:48:30 -0500

    Added JsonProperty to Models.AstronomyResponse.MoonPhase to resolve WUnderground JSON data "moon_phase" as the property name.

--- CreativeGurus.Weather.Wunderground/ResultModels/AstornomyResponse.cs
+++ CreativeGurus.Weather.Wunderground/ResultModels/AstornomyResponse.cs
@@ -1,7 +1,10 @@
-﻿namespace CreativeGurus.Weather.Wunderground.Models
+﻿using Newtonsoft.Json;
+
+namespace CreativeGurus.Weather.Wunderground.Models
 {
     public class AstronomyResponse
     {
+        [JsonProperty("moon_phase")]
         public MoonPhase MoonPhase { get; set; }
         public Response Response { get; set; }
     }
