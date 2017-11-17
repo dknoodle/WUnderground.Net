d35aeb98b8d575fad3ed3a671668b3d9b4e220fc
Kiril Iliev <cyril@abv.bg>
2017-11-17 10:42:17 +0200

    Added support for cities which return multiple results (extended the Response object with a Results collection)

--- CreativeGurus.Weather.Wunderground/Models/Response.cs
+++ CreativeGurus.Weather.Wunderground/Models/Response.cs
@@ -5,7 +5,12 @@ namespace CreativeGurus.Weather.Wunderground.Models
 {
 	public class Response
 	{
-		[JsonProperty("version")]
+        public Response()
+        {
+            this.Results = new List<ResultResponse>();
+        }
+
+        [JsonProperty("version")]
 		public string Version { get; set; }
 
 		[JsonProperty("termsofService")]
@@ -16,5 +21,8 @@ namespace CreativeGurus.Weather.Wunderground.Models
 
 		[JsonProperty("error")]
 		public Dictionary<string, string> Error { get; set; }
+
+        [JsonProperty("results")]
+        public IEnumerable<ResultResponse> Results { get; set; }
 	}
 }
---
+++ CreativeGurus.Weather.Wunderground/Models/ResultResponse.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace CreativeGurus.Weather.Wunderground.Models
+{
+    public class ResultResponse
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("country_iso3166")]
+        public string CountryISO { get; set; }
+
+        [JsonProperty("country_name")]
+        public string CountryName { get; set; }
+
+        [JsonProperty("zmw")]
+        public string ZMW { get; set; }
+
+        [JsonProperty("l")]
+        public string L { get; set; }
+    }
+}
